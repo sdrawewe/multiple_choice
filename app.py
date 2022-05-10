6c4c2bdc01b69e8b15871245ee73df18900f568d
suhendra <suhendra.eli@gmail.com>
2022-05-10 20:18:49 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 from question import Question
 
 question_prompts = [
-    "What color are Apple?\n(a) Red/Green\n(b) Purple\n(c) Orange\n\n",
-    "What color are Bananas?\n(a) Teal\n(b) Magenta\n(c) Yellow\n\n",
-    "What color are Strawberries?\n(a) Yellow\n(b) Red\n(c) Blue\n\n"
+    "What color are Oranges?\n(a) Red/Green\n(b) Purple\n(c) Orange\n\n",
+    "What color are Eggplants?\n(a) Teal\n(b) Magenta\n(c) Yellow\n\n",
+    "What color are Bananas?\n(a) Yellow\n(b) Red\n(c) Blue\n\n"
 ]
 
 questions = [
-    Question(question_prompts[0], "a"),
-    Question(question_prompts[1], "c"),
-    Question(question_prompts[2], "b"),
+    Question(question_prompts[0], "c"),
+    Question(question_prompts[1], "b"),
+    Question(question_prompts[2], ""),
 ]
 
 def run_test(questions):
